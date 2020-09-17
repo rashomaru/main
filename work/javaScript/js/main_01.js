6b78d1ad0daf2169e51909ba037e92bd4de066cc
rashomaru <70643932+rashomaru@users.noreply.github.com>
2020-09-18 01:40:33 +0900

    slideShow update

--- work/javaScript/js/main_01.js
+++ work/javaScript/js/main_01.js
@@ -1,3 +1,9 @@
+/*
+ * JavaScriptの理解を深めるため、あえてjQuery等のライブラリは使用しない。
+ * デザイン自体はjQueryの参考書のを参考にし作成。
+ */
+
+// スライドショーの関数
 function slideShow() {
   var $container = document.querySelector('#slideshow');
   var $slideGroup = $container.querySelector('.slideshow-wrapper');
@@ -13,6 +19,7 @@ function slideShow() {
   var duration = 500;
   var interval = 7500;
   var timer;
+  var slideControlFlg = true;  // 連打対応："goToSlide"関数が使用可能か判断するフラグ ture：使用可 false：使用不可
 
   // 各slideを横一面に並べる
   for(var i = 0; i < totalSlide; i++) {
@@ -25,12 +32,65 @@ function slideShow() {
 
   // コンテンツを表示する関数
   function goToSlide(index) {
-    $slideGroup.style.left = (- 100 * index) + '%';
+    if (slideControlFlg == true) {
+      var accumlatedValueOfLeft = 0;  // leftに設定する値。easing処理により蓄積させていく。
+      var startTime = Date.now();
+      var startPoint = Math.abs(Number($slideGroup.style.left.replace('%', '')));  // スライドラッパーの|スタート地点|を入れる
+      var targetPoint = 100 * index;
+      var distance = targetPoint - startPoint;
 
-    // 現在のインデックス情報を更新
-    currentIndex = index;
-    // ナビゲーションとインジケーターの情報を更新
-    updateNav();
+      // スライドのアニメーション実行関数
+      function slideAnimation() {
+        // この関数の処理が完了するまでgoToSlide禁止
+        slideControlFlg = false;
+
+        var currentTime = Date.now() - startTime;
+        // leftの数値を下記easing関数にて蓄積させていく
+        accumlatedValueOfLeft = easingEaseInOutExpo(currentTime, startPoint, distance, duration);
+
+        // スライドNext(小→大)の場合
+        if (startPoint < targetPoint) {
+          if (accumlatedValueOfLeft >= targetPoint) {
+            accumlatedValueOfLeft = targetPoint;
+          }
+        // スライドPrev(大→小)の場合
+        } else if (startPoint > targetPoint) {
+          if (accumlatedValueOfLeft <= targetPoint) {
+            accumlatedValueOfLeft = targetPoint;
+          }
+        }
+
+        // スライドラッパーのleftを設定
+        $slideGroup.style.left = (-accumlatedValueOfLeft) + '%';
+
+        // スライドラッパーの移動が完了したらgoToSlideの実行を許可する
+        var slideGroupLeftValue = Math.abs(Number($slideGroup.style.left.replace('%', '')));
+
+        if (slideGroupLeftValue == 0 || slideGroupLeftValue % 100 == 0) {
+          slideControlFlg = true;
+        }
+
+        // ループ処理設定
+        if (startPoint < targetPoint) {
+          if (accumlatedValueOfLeft < targetPoint) {
+            setTimeout(slideAnimation, 1);
+          }
+        } else if (startPoint > targetPoint) {
+          if (accumlatedValueOfLeft > targetPoint) {
+            setTimeout(slideAnimation, 1);
+          }
+        }
+      }
+
+      // スライドラッパーのアニメーション処理の実行
+      slideAnimation();
+      // 現在のインデックス情報を更新
+      currentIndex = index;
+      // ナビゲーションとインジケーターの情報を更新
+      updateNav();
+    } else {
+      return;
+    }
   }
 
   // ナビゲーションとインジケーターの情報を更新する関数
@@ -99,6 +159,20 @@ function slideShow() {
 
   // タイマー開始
   startTimer();
+}
+
+// easing "easeInOutExpo" jQuery公式を参考
+function easingEaseInOutExpo (currentTime, startPoint, distance, duration) {
+  if (currentTime == 0) {
+    return startPoint;
+  }
+  if (currentTime == duration) {
+    return startPoint + distance;
+  }
+  if ((currentTime /= duration / 2) < 1) {
+    return distance / 2 * Math.pow(2, 10 * (currentTime - 1)) + startPoint;
+  }
+  return distance / 2 * (-Math.pow(2, -10 * --currentTime) + 2) + startPoint;
 }
 
 window.addEventListener('load', function() {
