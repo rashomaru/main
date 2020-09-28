80ffe6e2d9e5bed1ddd21223ff0136f044e89310
rashomaru <70643932+rashomaru@users.noreply.github.com>
2020-09-29 00:07:02 +0900

    1st mission complete

--- work/javaScript/js/main_01.js
+++ work/javaScript/js/main_01.js
@@ -226,7 +226,7 @@ function tabPanel() {
   var $tabList = $container.querySelector('.tabs-nav');
   var $tabListAnchors = $tabList.querySelectorAll('a');
   var $tabPanels = $container.querySelectorAll('.tabs-panel');
-  
+
   // タブがクリックされたときの処理
   $tabList.addEventListener('click', function(event) {
     event.preventDefault();
@@ -262,8 +262,47 @@ function tabPanel() {
   $tabListAnchors[0].click();
 }
 
+/*
+ * smooth scroll
+ */
+function smoothSchroll() {
+  $back = document.querySelector('.back-to-top');
+
+  // ボタンにクリックイベントを設定
+  $back.addEventListener('click', function(event) {
+    event.preventDefault();
+    scrollToTop();
+  });
+}
+
+/* scroll用関数 */
+function scrollToTop() {
+  let x1 = x2 = x3 = 0;
+  let y1 = y2 = y3 = 0;
+  if (document.documentElement) {
+    x1 = document.documentElement.scrollLeft || 0;
+    y1 = document.documentElement.scrollTop || 0;
+  }
+  if (document.body) {
+    x2 = document.body.scrollLeft || 0;
+    y2 = document.body.scrollTop || 0;
+  }
+  x3 = window.scrollX || 0;
+  y3 = window.scrollY || 0;
+
+  let x = Math.max(x1, Math.max(x2, x3));
+  let y = Math.max(y1, Math.max(y2, y3));
+
+  window.scrollTo(Math.floor(x / 2), Math.floor(y / 2));
+
+  if (x > 0 || y > 0) {
+    window.setTimeout(scrollToTop, 30);
+  }
+}
+
 window.addEventListener('load', function() {
   slideShow();
   stickyHeader();
   tabPanel();
+  smoothSchroll();
 });
